b834bd08270334a383a2b8c7b08310339199d326
Grégoire Dubourg <gregoire.dubourg@gmail.com>
2013-05-28 22:19:13 +0200

    Added default config

---
+++ scripts/config.default.js
@@ -0,0 +1,12 @@
+// Rename this file 'config.js' and fill in your API key
+
+// App settings
+var config = {
+  apiKey: '' // Your API key
+};
+
+// Domino settings
+domino.settings({
+  verbose: true,
+  mergeRequests: true
+});
